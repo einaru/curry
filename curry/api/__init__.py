7fbe5edbf7249d23033c243a2005437886b064ea
Einar Uvsløkk <einar.uvslokk@gmail.com>
2014-10-24 13:04:51 +0200

    refactor api provider to new layout (finance.yahoo.com)

--- curry/api/__init__.py
+++ curry/api/__init__.py
@@ -193,56 +193,6 @@ class APIProvider:
         log.debug('*** End ***')
 
 
-class Yahoo(APIProvider):
-    id_ = 'finance.yahoo.com'
-    url = 'http://download.finance.yahoo.com/d/quotes.csv?s={}{}=X&f=l1'
-
-    def __init__(self, **kwargs):
-        APIProvider.__init__(self, **kwargs)
-        self.cache_file = get_cache_file(self.id_)
-
-    def get_exchange_rate(self, transaction, payment):
-        """Get the exchange rate for a currency pair (transaction
-        currency -> payment currency).
-
-        :param transaction: transaction (from) currency.
-        :param payment: payment (to) currency.
-
-        :returns: the exchange rate or raises an APIError.
-        """
-        rate = self.get_exchange_rate_from_cache(transaction, payment)
-
-        # If rate is None here the exchange rate was either not cached
-        # or its timestamp was too old, therefor we need to do a
-        # request for an up-to-date exchange rate.
-        if not rate:
-            url = self.url.format(transaction, payment)
-            log.debug('Request url: {}'.format(url))
-
-            r = requests.get(url)
-            self.dump_http_response(r)
-
-            # TODO:2014-10-23:einar: provide better user feedback.
-            # Should probably provide some sort of 'contact developer'
-            # functionality is implemented.
-            if r.status_code != 200:
-                raise APIError('Unknown API error happend.', self.id_)
-
-            rate = r.text
-
-            try:
-                rate = float(rate)
-            except ValueError:
-                raise APIError(rate, self.id_)
-
-            self.save_cache(transaction, payment, rate)
-
-        return rate
-
-
-register_api_provider(Yahoo.id_, Yahoo)
-
-
 class ExchangeRateAPI(APIProvider):
     id_ = 'exchangerate-api.com'
     url = 'http://www.exchangerate-api.com/{}/{}?k={}'
@@ -499,3 +449,6 @@ class OpenExchangeRates(APIProvider):
 
 
 register_api_provider(OpenExchangeRates.id_, OpenExchangeRates, ['api_key'])
+
+# Loads and registers API providers
+from curry.api.providers import *
